5abd8921eab3fcbd44d6544a34a9ad2a8797bfed
HugoLattard <55689599+Orphee1@users.noreply.github.com>
2020-01-16 21:35:57 +0100

    readme update

--- src/App.js
+++ src/App.js
@@ -7,99 +7,117 @@ import "./App.css";
 import Axios from "axios";
 
 function App() {
-  const [movies, setMovies] = useState();
-  const [loading, setLoading] = useState(true);
-  const [selectedTab, setSelectedTab] = useState("popular");
+      const [movies, setMovies] = useState();
+      const [loading, setLoading] = useState(true);
+      const [selectedTab, setSelectedTab] = useState("popular");
 
-  const fetchData = async () => {
-    try {
-      const response = await Axios.get(
-        "https://api-allocine.herokuapp.com/api/movies/" + selectedTab
+      const fetchData = async () => {
+            try {
+                  const response = await Axios.get(
+                        "https://api-allocine.herokuapp.com/api/movies/" +
+                              selectedTab
+                  );
+                  setMovies(response.data.results);
+                  setLoading(false);
+            } catch (e) {
+                  alert("An error occured");
+            }
+      };
+
+      useEffect(() => {
+            fetchData(selectedTab);
+      }, [selectedTab]);
+
+      return (
+            <header className="yellow">
+                  <div className="container yellow header pv-32">
+                        <img src={Logo} alt="logo" className="logo" />
+                  </div>
+                  <section className="grey">
+                        <div className="top-bar grey">
+                              <div className="container">
+                                    <ul className="menu">
+                                          <li
+                                                className={
+                                                      selectedTab === "popular"
+                                                            ? "selected"
+                                                            : "unselected"
+                                                }
+                                                onClick={() => {
+                                                      setSelectedTab("popular");
+                                                      setLoading(false);
+                                                }}
+                                          >
+                                                Popular Movies
+                                          </li>
+                                          <li
+                                                className={
+                                                      selectedTab === "upcoming"
+                                                            ? "selected"
+                                                            : "unselected"
+                                                }
+                                                onClick={() => {
+                                                      setSelectedTab(
+                                                            "upcoming"
+                                                      );
+                                                      setLoading(false);
+                                                }}
+                                          >
+                                                Upcoming Movies
+                                          </li>
+                                          <li
+                                                className={
+                                                      selectedTab ===
+                                                      "top_rated"
+                                                            ? "selected"
+                                                            : "unselected"
+                                                }
+                                                onClick={() => {
+                                                      setSelectedTab(
+                                                            "top_rated"
+                                                      );
+                                                      setLoading(false);
+                                                }}
+                                          >
+                                                Top Rated Movies
+                                          </li>
+                                    </ul>
+                              </div>
+                        </div>
+                        <div className="bottom grey">
+                              <div className="container card-container">
+                                    {loading === true ? (
+                                          <div class="lds-roller">
+                                                <div></div>
+                                                <div></div>
+                                                <div></div>
+                                                <div></div>
+                                                <div></div>
+                                          </div>
+                                    ) : (
+                                          movies.map((movie, index) => {
+                                                return (
+                                                      <Card
+                                                            key={index}
+                                                            src={
+                                                                  movie.poster_path
+                                                            }
+                                                            title={movie.title}
+                                                            date={
+                                                                  movie.release_date
+                                                            }
+                                                            story={
+                                                                  movie.overview
+                                                            }
+                                                      />
+                                                );
+                                          })
+                                    )}
+                              </div>
+                        </div>
+                  </section>
+            </header>
       );
-      setMovies(response.data.results);
-      setLoading(false);
-    } catch (e) {
-      alert("An error occured");
-    }
-  };
-
-  useEffect(() => {
-    fetchData(selectedTab);
-  }, [selectedTab]);
-
-  return (
-    <header className="yellow">
-      <div className="container yellow header pv-32">
-        <img src={Logo} alt="logo" className="logo" />
-      </div>
-      <section className="grey">
-        <div className="top-bar grey">
-          <div className="container">
-            <ul className="menu">
-              <li
-                className={
-                  selectedTab === "popular" ? "selected" : "unselected"
-                }
-                onClick={() => {
-                  setSelectedTab("popular");
-                  setLoading(false);
-                }}
-              >
-                Popular Movies
-              </li>
-              <li
-                className={
-                  selectedTab === "upcoming" ? "selected" : "unselected"
-                }
-                onClick={() => {
-                  setSelectedTab("upcoming");
-                  setLoading(false);
-                }}
-              >
-                Upcoming Movies
-              </li>
-              <li
-                className={
-                  selectedTab === "top_rated" ? "selected" : "unselected"
-                }
-                onClick={() => {
-                  setSelectedTab("top_rated");
-                  setLoading(false);
-                }}
-              >
-                Top Rated Movies
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="bottom grey">
-          <div className="container card-container">
-            {loading === true ? (
-              <div class="lds-roller">
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-              </div>
-            ) : (
-              movies.map((movie, index) => {
-                return (
-                  <Card
-                    key={index}
-                    src={movie.poster_path}
-                    title={movie.title}
-                    date={movie.release_date}
-                    story={movie.overview}
-                  />
-                );
-              })
-            )}
-          </div>
-        </div>
-      </section>
-    </header>
-  );
 }
 
 export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,23 +2,26 @@ import React, { useState, useEffect } from "react";
 import "../App.css";
 
 const Card = props => {
-  return (
-    <div className="card">
-      <div className="bloc-image">
-        <img
-          src={"https://image.tmdb.org/t/p/w370_and_h556_bestv2" + props.src}
-          alt="poster"
-        />
-      </div>
-      <div className="bloc-news m-10">
-        <div className="bloc-title m-5">
-          <div className="title m-5">{props.title}</div>
-          <div className="date m-5">{props.date}</div>
-        </div>
-        <div className="bloc-text">{props.story}</div>
-      </div>
-    </div>
-  );
+      return (
+            <div className="card">
+                  <div className="bloc-image">
+                        <img
+                              src={
+                                    "https://image.tmdb.org/t/p/w370_and_h556_bestv2" +
+                                    props.src
+                              }
+                              alt="poster"
+                        />
+                  </div>
+                  <div className="bloc-news m-10">
+                        <div className="bloc-title m-5">
+                              <div className="title m-5">{props.title}</div>
+                              <div className="date m-5">{props.date}</div>
+                        </div>
+                        <div className="bloc-text">{props.story}</div>
+                  </div>
+            </div>
+      );
 };
 
 export default Card;
